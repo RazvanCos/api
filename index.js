ba3590219b7f0d4d731f08e8f368053b28c78101
RazvanCos <urc26@yahoo.com>
2024-08-03 19:12:23 +0300

    add mongodb connection

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server Updated");
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello from Node API");
-})
+mongoose.connect('mongodb+srv://admin:userPassword1234@backenddb.8dwv4kq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
+    .then(() => {
+        console.log('Connected to the database!');
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch(() => {
+        console.log("Connection failed")
+    });
