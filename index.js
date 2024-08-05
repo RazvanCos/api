fb17d64813d894ead26b00282759eb0d55bcc45a
RazvanCos <urc26@yahoo.com>
2024-08-05 11:11:17 +0300

    add api calls

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import router from './routes/product.route.js';
 
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use("/api/products", router);
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API Server Updated");
 });
+
+// database connection
 
 mongoose.connect('mongodb+srv://admin:userPassword1234@backenddb.8dwv4kq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
     .then(() => {
